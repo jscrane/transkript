7d7c43e5b3f3ab502b5450e7f0cb04c29e467217
Stephen Crane <jscrane@gmail.com>
2018-07-03 14:30:16 +0100

    add logging config. add dependency on tools.cli.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject transkript "0.1.8-SNAPSHOT"
+(defproject transkript "0.1.9-SNAPSHOT"
   :description "A Clojure client API for Transkribus"
   :url "https://github.com/jscrane/transkript"
   :license {:name "Eclipse Public License 2.0"
             :url  "http://www.eclipse.org/legal/epl-v20.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.data "0.1.1"]
-                 [eu.transkribus/TranskribusClient "0.0.2"]])
+                 [eu.transkribus/TranskribusClient "0.0.2"]
+                 [org.clojure/tools.cli "0.3.7"]])
