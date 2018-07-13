c191b59fef2612c4feba028803e145851752399a
Stephen Crane <jscrane@gmail.com>
2018-07-13 09:12:08 +0100

    add manifest section so JAI works standalone

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject transkript "0.1.11-SNAPSHOT"
+(defproject transkript "0.1.12-SNAPSHOT"
   :description "A Clojure client API for Transkribus"
   :url "https://github.com/jscrane/transkript"
   :license {:name "Eclipse Public License 2.0"
@@ -7,4 +7,13 @@
                  [org.clojure/java.data "0.1.1"]
                  [eu.transkribus/TranskribusClient "0.0.2"]
                  [org.clojure/tools.cli "0.3.7"]]
-  :aot [eval-models, run-ocr])
+  :aot [eval-models, run-ocr]
+  :manifest {
+             "Specification-Title"    "Java Advanced Imaging Image I/O Tools"
+             "Specification-Version"  "1.1"
+             "Specification-Vendor"   "Sun Microsystems, Inc."
+             "Implementation-Title"   "com.sun.media.imageio"
+             "Implementation-Version" "1.1"
+             "Implementation-Vendor"  "Sun Microsystems, Inc."
+             }
+  )
