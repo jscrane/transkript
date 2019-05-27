c175ec37eef89360b178f105fe3952006319b273
Stephen Crane <jscrane@gmail.com>
2019-05-27 15:17:10 +0100

    update versions and specify mvn repo for Transkribus artifacts

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
-(defproject transkript "0.1.12-SNAPSHOT"
+(defproject transkript "0.1.13-SNAPSHOT"
   :description "A Clojure client API for Transkribus"
   :url "https://github.com/jscrane/transkript"
   :license {:name "Eclipse Public License 2.0"
             :url  "http://www.eclipse.org/legal/epl-v20.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/java.data "0.1.1"]
-                 [eu.transkribus/TranskribusClient "0.0.2"]
-                 [org.clojure/tools.cli "0.3.7"]]
+                 [eu.transkribus/TranskribusCore "0.1.9"]
+                 [eu.transkribus/TranskribusClient "0.1.0"]
+                 [org.clojure/tools.cli "0.4.2"]]
+  :repositories [["dbis-halvar.uibk.ac.at" "https://dbis-halvar.uibk.ac.at/artifactory/libs-release/"]]
   :aot [eval-models, run-ocr]
   :manifest {
              "Specification-Title"    "Java Advanced Imaging Image I/O Tools"
